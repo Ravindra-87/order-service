bd70faa9e515ef65f99b318974d9c37e7dd35516
ravikumar <ravilearn87@gmail.com>
2025-06-02 02:18:52 +0530

    commit 6

--- src/main/java/com/ravi/OrderServiceApplication.java
+++ src/main/java/com/ravi/OrderServiceApplication.java
@@ -13,7 +13,7 @@ public class OrderServiceApplication {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8081") // required for uriBuilder to work
+                .baseUrl("http://34.72.191.12:30081/") // required for uriBuilder to work
                 .build();
     }
 
